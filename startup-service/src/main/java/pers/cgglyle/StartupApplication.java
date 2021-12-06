14e0e98a818ce4b6b377fbd0f734ee40504ad2b6
lyle <86522347+cgglyle@users.noreply.github.com>
2021-12-06 14:23:15 +0800

    初始构建项目框架

---
+++ startup-service/src/main/java/pers/cgglyle/StartupApplication.java
@@ -0,0 +1,15 @@
+package pers.cgglyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cgglyle
+ * @date 2021/12/6
+ */
+@SpringBootApplication
+public class StartupApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartupApplication.class, args);
+    }
+}
