ba63f8e949d3980b2dd63f76be3946c08f7deef5
lyle <86522347+cgglyle@users.noreply.github.com>
2021-12-08 10:24:48 +0800

    add：Mybatis配置文件、ResultConfig配置文件（为实现）

---
+++ common/src/main/java/pers/cgglyle/util/config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package pers.cgglyle.util.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Mybatis-plus配置文类
+ *
+ * @author cgglyle
+ * @date 2021/12/7
+ */
+@Configuration
+@MapperScan("pers.cgglyle.**.mapper")
+public class MybatisPlusConfig {
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
---
+++ service/service-account/src/main/java/pers/cgglyle/service/acconut/config/ResultConfig.java
@@ -0,0 +1,11 @@
+package pers.cgglyle.service.acconut.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author cgglyle
+ * @date 2021/12/7
+ */
+@Configuration
+public class ResultConfig {
+}
